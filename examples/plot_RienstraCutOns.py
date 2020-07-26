8979e7cda2019b20f6ad0fd4372a6cec5c1bc16f
ssackMWL <68465750+ssackMWL@users.noreply.github.com>
2020-07-26 21:55:38 +0200

    Update plot_RienstraCutOns.py

--- examples/plot_RienstraCutOns.py
+++ examples/plot_RienstraCutOns.py
@@ -8,11 +8,7 @@ esults correspondto Rienstra "Fundamentals of Duct Acoustics" (Figures 7 and 8).
 """
 
 # %%
-# .. image:: ./docs/source/image/ripples.jpg
-#    :width: 800
-
-# %%
-# .. image:: ./source/image/ripples.jpg
+# .. image:: image/ripples.jpg
 #    :width: 800
 
 
